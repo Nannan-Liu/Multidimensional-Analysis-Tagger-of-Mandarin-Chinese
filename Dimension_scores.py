366f3c553f1b3ec6e511fabbd4b7921079a2f660
nnl93 <liunannan.bfsumun@gmail.com>
2020-10-10 17:39:55 +0800

    Update Dimension_scores.py

--- Dimension_scores.py
+++ Dimension_scores.py
@@ -55,10 +55,9 @@ text_list=list(df.index.values)
 
 df.head()
 
-
-
 def dimension1(text): 
-    return df.loc[text, 'question']+df.loc[text, 'particle']+df.loc[text, 'interrogative']+df.loc[text, 'exclamation']+df.loc[text, 'SPP']+df.loc[text, 'WH']+df.loc[text, 'PUBV']+df.loc[text, 'mono_negation']+df.loc[text, 'FPP']+df.loc[text, 'honourifics']+df.loc[text, 'INPR']+df.loc[text, 'PRIV']-df.loc[text, 'classical_func']
+    return df.loc[text, 'question']+df.loc[text, 'particle']+df.loc[text, 'exclamation']+df.loc[text, 'SPP']+df.loc[text, 'interrogative']+df.loc[text, 'PUBV']+df.loc[text, 'WH']+df.loc[text, 'mono_negation']+df.loc[text, 'person']+df.loc[text, 'honourifics']+df.loc[text, 'FPP']+df.loc[text, 'INPR']+df.loc[text, 'emotion']-df.loc[text, 'AWL']
+
 
 dimension1_scores=[]
 for text in text_list:
@@ -67,7 +66,7 @@ for text in text_list:
 
 
 def dimension2(text): 
-    return df.loc[text, 'lexical_density']+df.loc[text, 'NOMZ']+df.loc[text, 'performative_verb']+df.loc[text, 'disyllabic_words']+df.loc[text, 'HSK_6']+df.loc[text, 'abstract']+df.loc[text, 'PHC']+df.loc[text, 'consecutive_nouns']+df.loc[text, 'aux_adj']+df.loc[text, 'AWL']+df.loc[text, 'BPIN']-df.loc[text, 'classifier']-df.loc[text, 'person']-df.loc[text, 'mono_verbs']-df.loc[text, 'unique']
+    return df.loc[text, 'descriptive']+df.loc[text, 'imperfect']+df.loc[text, 'modify_marker_di']+df.loc[text, 'simile']+df.loc[text, 'PEAS']+df.loc[text, 'onomatopoeia']+df.loc[text, 'TPP']+df.loc[text, 'classifier']+df.loc[text, 'mono_verbs']+df.loc[text, 'SMP']+df.loc[text, 'modify_marker_de']
 
 
 dimension2_scores=[]
@@ -76,7 +75,7 @@ for text in text_list:
 
 
 def dimension3(text): 
-    return df.loc[text, 'BE']+df.loc[text, 'modify_adv']+df.loc[text, 'COND']+df.loc[text, 'RB']+df.loc[text, 'EX']+df.loc[text, 'DEMP']+df.loc[text, 'AMP']+df.loc[text, 'HDG']+df.loc[text, 'DWNT']+df.loc[text, 'di_negation']+df.loc[text, 'HSK_1']+df.loc[text, 'HSK_3']+df.loc[text, 'other_personal']-df.loc[text, 'noun']
+    return df.loc[text, 'BE']+df.loc[text, 'COND']+df.loc[text, 'modify_adv']+df.loc[text, 'AMP']+df.loc[text, 'EX']+df.loc[text, 'HDG']+df.loc[text, 'DWNT']+df.loc[text, 'RB']+df.loc[text, 'di_negation']+df.loc[text, 'HSK_3']+df.loc[text, 'DEMP']+df.loc[text, 'PRIV']+df.loc[text, 'HSK_1']+df.loc[text, 'other_personal']-df.loc[text, 'noun']
 
 
 dimension3_scores=[]
@@ -85,7 +84,7 @@ for text in text_list:
 
 
 def dimension4(text): 
-    return df.loc[text, 'modify_marker_di']+df.loc[text, 'imperfect']+df.loc[text, 'descriptive']+df.loc[text, 'simile']+df.loc[text, 'PEAS']+df.loc[text, 'TPP']+df.loc[text, 'SMP']+df.loc[text, 'modify_marker_de']
+    return df.loc[text, 'ACL']+df.loc[text, 'ASL']+df.loc[text, 'ASL_std']+df.loc[text, 'PHC']+df.loc[text, 'BPIN']-df.loc[text, 'unique']-df.loc[text, 'intransitive']-df.loc[text, 'di_verbs']
 
 
 dimension4_scores=[]
@@ -95,7 +94,7 @@ for text in text_list:
 
 
 def dimension5(text): 
-    return df.loc[text, 'ACL_std']+df.loc[text, 'ACL']+df.loc[text, 'ASL']
+    return df.loc[text, 'classical_func']+df.loc[text, 'classical_syntax']-df.loc[text, 'aux_adj']-df.loc[text, 'lexical_density']-df.loc[text, 'NOMZ']-df.loc[text, 'disyllabic_words']
 
 
 
